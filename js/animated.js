796aa59ce565f299a52c8c3f6944c6347fbac2ab
yanchuk <yanchukserhiy96@gmail.com>
2016-12-22 21:41:40 +0300

    add jquery animation + portfolio masonry

--- js/animated.js
+++ js/animated.js
@@ -9,3 +9,17 @@ $('.hvr-push').addClass('animated jello');
 $('#slideH1one').addClass('animated tada');
 
 
+// aniamte secondtext
+$(document).ready(function () {
+        var objToStick = $(".box"); //Получаем нужный объект
+        var topOfObjToStick = $(objToStick).offset().top; //Получаем начальное расположение нашего блока
+
+        $(window).scroll(function () {
+            var windowScroll = $(window).scrollTop(); //Получаем величину, показывающую на сколько прокручено окно
+
+            if (windowScroll = topOfObjToStick) { // Если прокрутили больше, чем расстояние до блока, то приклеиваем его
+                $(objToStick).addClass('animated slideInUp');
+           
+            };
+        });
+    });
