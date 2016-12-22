af509414b65b451ac7f31178db295dde1ae9fca0
yanchuk <yanchukserhiy96@gmail.com>
2016-12-22 19:43:02 +0300

    adaptation nav + slider

---
+++ js/animated.js
@@ -0,0 +1,11 @@
+ $('.navbar img').addClass('animated slideInDown');
+$('.nav').addClass('animated slideInRight');
+
+
+$('#sliderImg').addClass('animated rollIn');
+
+$('.hvr-push').addClass('animated jello');
+
+$('#slideH1one').addClass('animated tada');
+
+
